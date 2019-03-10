da62333a02604c7d998183fbc5f46b14e131c147
Denis Obydennykh <denis.obydennykh@gmail.com>
2019-03-10 04:53:48 +0100

    more debug

--- index.ts
+++ index.ts
@@ -2,34 +2,82 @@ import {exec} from "child_process";
 
 const CDP = require('chrome-remote-interface');
 
+const debuggerEvents = [
+    ['paused', 'reason'],
+    'breakpointResolved',
+    'resumed',
+    'scriptFailedToParse',
+//    ['scriptParsed', 'url'],
+];
+
+
+async function delay(time) {
+    console.log(`Delay ${time}`);
+    await new Promise(resolve => setTimeout(resolve, time));
+}
+
 async function main() {
     const nodeCommand = 'git clean -fd && git reset --hard && node --inspect-brk=9222 ./node_modules/@angular/cli/bin/ng g prettify-schematic:my-schematic';
-    exec(nodeCommand, {
-            cwd: '../test-prettify-schematics'
-        },
-        (error, stdout, stderr) => console.log('Exec: ', error, stdout, stderr));
 
-    await new Promise(resolve => setTimeout(resolve, 5000));
+const doExec = true;
+    if (doExec) {
+        exec(nodeCommand, {
+                cwd: '../test-prettify-schematics'
+            },
+            (error, stdout, stderr) =>
+                console.log('Exec: ', error ? `${error} : ${stderr}` : 'XXX' /*'Executed successfully!'*/));
+    }
 
-    CDP(async (client) => {
+    await delay(2000);
+
+    CDP(async (client: any) => {
         debugger;
 
         const {Debugger, Runtime} = client;
         try {
-            client.Debugger.paused(() => {
-                console.log('Debugger paused');
-                // client.Debugger.resume();
-                client.close();
+
+            debuggerEvents.forEach(event => {
+                const eventName = Array.isArray(event) ? event[0] : event;
+                const displayProperty = Array.isArray(event) && event[1];
+                client.Debugger[eventName]((eventData: any) => console.log.apply(console, ['Debugger ' + event, displayProperty ? eventData[displayProperty] : eventData]));
             });
+
+            Debugger.paused(() => {
+                console.log('Debugger paused');
+                client.Debugger.resume();
+                // client.close();
+            });
+
+
+            client.Debugger.scriptParsed((script: any) => {
+                console.log(`Debugger scriptParsed: ${script.url}`);
+                if (script.url.indexOf('file-system-engine-host-base') > 0) {
+                    debugger;
+                }
+            });
+
             console.log('before runIfWaitingForDebugger');
-            await client.Runtime.runIfWaitingForDebugger();
+
+            // await delay(1000);
             console.log('after runIfWaitingForDebugger');
-            await client.Debugger.enable();
-            console.log('after runIfWaitingForDebugger');
+            // await delay(1000);
+            await Runtime.runIfWaitingForDebugger();
+            await Debugger.enable();
+
+            Debugger.setBreakpointByUrl({
+                lineNumber: 0,
+                urlRegex: 'file-system-engine-host-base'
+            }, (...result: any[]) => {
+                console.log(result);
+                debugger;
+            });
+
+            //            console.log('after runIfWaitingForDebugger');
         } catch (err) {
             console.error(err);
         } finally {
-            client.close();
+            console.log('finally - close');
+            // client.close();
         }
     }).on('error', (err) => {
         console.error(err);
@@ -38,3 +86,6 @@ async function main() {
 }
 
 main();
+
+
+// angular-cli/packages/angular_devkit/schematics/tools/file-system-engine-host-base.ts
