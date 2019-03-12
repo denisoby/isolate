cb1654c11ae5dbf6822407f4128bb443ba5775ec
Denis Obydennykh <denis.obydennykh@gmail.com>
2019-03-12 09:20:59 +0100

    minor changes

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ const debuggerEvents = [
 ];
 
 
-async function delay(time) {
+async function delay(time: number) {
     console.log(`Delay ${time}`);
     await new Promise(resolve => setTimeout(resolve, time));
 }
@@ -19,7 +19,7 @@ async function delay(time) {
 async function main() {
     const nodeCommand = 'git clean -fd && git reset --hard && node --inspect-brk=9222 ./node_modules/@angular/cli/bin/ng g prettify-schematic:my-schematic';
 
-const doExec = true;
+    const doExec = true;
     if (doExec) {
         exec(nodeCommand, {
                 cwd: '../test-prettify-schematics'
@@ -31,8 +31,6 @@ const doExec = true;
     await delay(2000);
 
     CDP(async (client: any) => {
-        debugger;
-
         const {Debugger, Runtime} = client;
         try {
 
@@ -42,16 +40,26 @@ const doExec = true;
                 client.Debugger[eventName]((eventData: any) => console.log.apply(console, ['Debugger ' + event, displayProperty ? eventData[displayProperty] : eventData]));
             });
 
-            Debugger.paused(() => {
-                console.log('Debugger paused');
+            Debugger.paused((event: any) => {
+                if (event.reason === "Break on start") {
+                    return client.Debugger.resume();
+                }
+                debugger;
+                console.log('Debugger paused: ', event.reason);
+                client.Debugger.resume();
+                // client.close();
+            });
+
+            Debugger.breakpointResolved((event: any) => {
+                console.log('breakpointResolved: ', event);
                 client.Debugger.resume();
                 // client.close();
             });
 
 
             client.Debugger.scriptParsed((script: any) => {
-                console.log(`Debugger scriptParsed: ${script.url}`);
-                if (script.url.indexOf('file-system-engine-host-base') > 0) {
+                // console.log(`Debugger scriptParsed: ${script.url}`);
+                if (script.url.indexOf('base.js') > 0) {
                     debugger;
                 }
             });
@@ -79,7 +87,7 @@ const doExec = true;
             console.log('finally - close');
             // client.close();
         }
-    }).on('error', (err) => {
+    }).on('error', (err: any) => {
         console.error(err);
     });
 
@@ -91,3 +99,30 @@ main();
 // angular-cli/packages/angular_devkit/schematics/tools/file-system-engine-host-base.ts
 // angular-cli/packages/angular_devkit/schematics/tools/export-ref.ts
 // angular-cli/packages/angular/cli/commands/generate-impl.ts
+
+
+// script parsed
+// let x = {
+//     "scriptId": "570",
+//     "url": "/Users/dobydennykh/dev/test-prettify-schematics/node_modules/fsevents/node_modules/are-we-there-yet/tracker-base.js",
+//     "startLine": 0,
+//     "startColumn": 0,
+//     "endLine": 12,
+//     "endColumn": 3,
+//     "executionContextId": 1,
+//     "hash": "08328883401BE4171FB7B2461292B5068C09E644",
+//     "isLiveEdit": false,
+//     "sourceMapURL": "",
+//     "hasSourceURL": false,
+//     "isModule": false,
+//     "length": 340,
+//     "stackTrace": {
+//         "callFrames": [{
+//             "functionName": "createScript",
+//             "scriptId": "46",
+//             "url": "vm.js",
+//             "lineNumber": 79,
+//             "columnNumber": 9
+//         }]
+//     }
+// }
